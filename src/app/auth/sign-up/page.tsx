6c07f93d4c12802e9d17cb0f1341fb8d9dad1cbc
Andres Rubio <andresfra92@gmail.com>
2025-08-19 20:11:53 +0200

    fixing more

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -10,22 +10,22 @@ import { logger } from '@/app/utils/logger';
 export default function SignUpPage() {
   const router = useRouter();
 
-  const signUpMutation = useMutation({
-    mutationFn: async () => {
-      // await authSignUpCS(email, password, username, name);
-    },
-    onSuccess: () => {
-      // notifications.show({
-      //   title: "Success",
-      //   message: 'Account created succesfully, please verify email to use the app.',
-      //   color: "green"
-      // });
-      router.push('/');
-    },
-    onError: (error: unknown) => {
-      console.error(error);
-    },
-  })
+  // const signUpMutation = useMutation({
+  //   mutationFn: async () => {
+  //     // await authSignUpCS(email, password, username, name);
+  //   },
+  //   onSuccess: () => {
+  //     // notifications.show({
+  //     //   title: "Success",
+  //     //   message: 'Account created succesfully, please verify email to use the app.',
+  //     //   color: "green"
+  //     // });
+  //     router.push('/');
+  //   },
+  //   onError: (error: unknown) => {
+  //     console.error(error);
+  //   },
+  // })
 
   return (
     <div>Signup todo</div>
