a6f4269580d462e514c044ce18db6e7dd18e3383
Andres Rubio <andresfra92@gmail.com>
2025-08-19 20:08:55 +0200

    fixing parameter

--- src/app/session/[id]/page.tsx
+++ src/app/session/[id]/page.tsx
@@ -3,9 +3,10 @@
 import { sessionGetByIdSS } from "@/app/common/session/service/sessionGetByIdSS";
 import SessionView from "./components/session-view/SessionView";
 
-export default async function SessionDetailPage({ params }: { params: { id: string } }) {
+export default async function SessionDetailPage({ params, }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
 
-  const session = await sessionGetByIdSS(params.id);
+  const session = await sessionGetByIdSS(id);
 
   return (
     <SessionView session={session} />
